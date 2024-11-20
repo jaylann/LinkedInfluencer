5e89e9d5c36a0e93afb87bce0aefba5edc51aa74
jaylann <Justin.Lanfermann@hotmail.com>
2024-11-20 10:50:06 +0100

    fix documentation for RSSFeed

--- src/models/RSSFeed.py
+++ src/models/RSSFeed.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel, Field
 
+#==============================================================
+# This RSSFeed class represents YOUR RSS Feed.
+# Change the Field defaults accordingly.
+#
+# On the first run the S3Service will use
+# these defaults to create your RSS Feed
+#==============================================================
+
 
 class RSSFeed(BaseModel):
-    """Represents the structure of an RSS feed."""
+    """Represents the structure of YOUR RSS feed."""
     title: str = Field(default="Justin Lanfermann LinkedIn Feed")
     link: str = Field(default="https://www.linkedin.com/in/justin-lanfermann-07352124b/")
     description: str = Field(default="Here I post the most exciting pieces of news in the tech world!")
